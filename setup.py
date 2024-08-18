3a9772d566893a109f67865ff56f002aa07cd6ac
WilliamZhu <allwefantasy@gmail.com>
2024-08-18 18:36:57 +0800

    auto_coder_pre_121_chat_action.yml_6005ea99ed5920e96c7435543c71e727

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name='code-dataset',
-    version='0.1.0',
+    version='0.1.1',
     author='WilliamZhu',
     author_email='allwefantsy@gmail.com',
     description='A tool for managing code datasets',
